6062932c27887281e76365524899c8116bfbf640
linyang <lin2798003@sina.com>
2018-07-24 18:01:25 +0800

    提供私有类库

--- application/modules/Test/controllers/Log.php
+++ application/modules/Test/controllers/Log.php
@@ -1,8 +1,9 @@
 <?php
 
+use Base\Log\Log;
+use Base\Log\LogAction;
 use Myaf\Core\G;
 use Myaf\Core\WebController;
-use Myaf\Log\Log;
 
 /**
  * Created by PhpStorm.
@@ -12,12 +13,24 @@ use Myaf\Log\Log;
  */
 class LogController extends WebController
 {
+    /**
+     * 打印业务日志
+     */
     public function indexAction()
     {
         $uid = uniqid();
-        Log::debug(G::route(), $uid, 200, 'debug', ['orderSn' => time()], ['time' => time()], ['version' => PHP_VERSION]);
-        Log::warn(G::route(), $uid, 400, 'warn', ['orderSn' => time()], ['time' => time()], ['version' => PHP_VERSION]);
-        Log::error(G::route(), $uid, 500, 'debug', ['orderSn' => time()], ['time' => time()], ['version' => PHP_VERSION]);
-        Log::fatal(G::route(), $uid, 600, 'debug', ['orderSn' => time()], ['time' => time()], ['version' => PHP_VERSION]);
+        Log::debug(G::route(), $uid, 200, 'debug', ['orderSn' => time()]);
+        Log::warn(G::route(), $uid, 400, 'warn', ['orderSn' => time()]);
+        Log::error(G::route(), $uid, 500, 'debug', ['orderSn' => time()]);
+        Log::fatal(G::route(), $uid, 600, 'debug', ['orderSn' => time()]);
+    }
+
+    /**
+     * 统计类日志
+     */
+    public function statAction()
+    {
+        $uid = uniqid();
+        LogAction::save($uid, 'test-action', ['a' => 1, 'b' => 2]);
     }
 }
